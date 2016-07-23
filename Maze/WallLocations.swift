89953fab326db4961c7bf43c55a1eea901fd09c3
Troy Maletz <maletzt@gmail.com>
2016-07-23 16:57:59 -0400

    Initial Submission
    Initial submission for RobotMaze 2 project

---
+++ Maze/WallLocations.swift
@@ -0,0 +1,78 @@
+//
+//  CollectingDataAboutWalls.swift
+//  Maze
+//
+//  Created by Gabrielle Miller-Messner on 11/6/15.
+//  Copyright © 2015 Udacity, Inc. All rights reserved.
+//
+
+import Foundation
+
+extension ControlCenter {
+    
+    func isFacingWall(robot: ComplexRobotObject, direction: MazeDirection) -> Bool {
+        
+        let cell = mazeController.currentCell(robot)
+        var isWall: Bool = false
+        
+        switch(direction) {
+        case .Up:
+            if cell.top {
+                isWall = true
+            }
+        case .Down:
+            if cell.bottom {
+                isWall = true
+            }
+            
+        case .Left:
+            if cell.left {
+                isWall = true
+            }
+            
+            
+        case .Right:
+            if cell.right {
+                isWall = true
+            }
+            
+            
+        }
+        
+        
+        
+        return isWall
+    }
+    
+    func checkWalls(robot:ComplexRobotObject) -> (up: Bool, right: Bool, down: Bool, left: Bool, numberOfWalls: Int) {
+        var numberOfWalls = 0
+        let cell = mazeController.currentCell(robot)
+        
+        // Check is there is a wall at the top of the current cell
+        let isWallUp = cell.top
+        if isWallUp {
+            numberOfWalls += 1
+        }
+        
+        // Check if there is a wall to the right of the current cell
+        let isWallRight = cell.right
+        if isWallRight {
+            numberOfWalls += 1
+        }
+        
+        let isWallLeft = cell.left
+        if isWallLeft {
+            numberOfWalls += 1
+        }
+        
+        
+        let isWallBottom = cell.bottom
+        if isWallBottom {
+            numberOfWalls += 1
+        }
+        
+        
+        
+        return (isWallUp, isWallRight, isWallBottom, isWallLeft, numberOfWalls)
+    }
+}
